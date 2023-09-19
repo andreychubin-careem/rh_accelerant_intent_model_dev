7e74897c5ab6e60823e11941720746231be30a4c
andreychubin <andrey.chubin@careem.com>
2023-09-19 23:49:33 +0400

    switched to polars

---
+++ intent_model/preprocessing/_utils.py
@@ -0,0 +1,11 @@
+TZ_DICT = {
+    'United Arab Emirates': 'Asia/Dubai',
+    'Jordan': 'Asia/Amman'
+}
+
+BOUNDS_DICT = {
+    'United Arab Emirates': [(22.5, 27), (52.2, 56.5)],
+    'Jordan': [(30, 33), (34, 37)]
+}
+
+VALID_SERVICE_AREA_IDS = ('1', '21', '64', '68', '111', '87', '49', '47')
